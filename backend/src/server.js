4cae6ecc0f148b44d019845290937c19c76af371
urja <urjagajera12@gamil.com>
2026-08-07 13:56:58 +0530

    implement user authentication system

--- backend/src/server.js
+++
@@ -1,8 +0,0 @@
-// src/server.js
-import app from './app.js';
-
-const PORT = process.env.PORT || 3001;
-
-app.listen(PORT, () => {
-  console.log(`[Server] Express backend listening on http://localhost:${PORT}`);
-});
